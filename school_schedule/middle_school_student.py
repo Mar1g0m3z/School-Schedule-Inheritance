29c6c29be63726c34529f4c43d8b89c2c7f3afd0
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-04-02 11:32:22 -0700

    Implementing MiddleSchoolStudent class and writing tests for that class.

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,15 @@
 from .student import Student
 
-# add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+    
+    def has_transportation_message(self):
+        has_message = "gets" if self.gets_transportation else "doesn't gets"
+        return f"{self.name} {has_message} transportation"
+
+    def summary(self):
+        student_summary = super().summary()
+        transportation_message = self.has_transportation_message()
+        return "\n".join((student_summary, transportation_message))
